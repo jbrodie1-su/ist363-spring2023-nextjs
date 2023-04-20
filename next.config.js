0a2b4f5889d773911110a4f6fc0f8eb5b281a2af
jbrodie-su <jbrodie@syr.edu>
2023-04-20 14:45:53 -0400

    configured dynamic route

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
+// paste into next.config.js in your root project directory
 module.exports = {
   images: {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'ist-363-subaru-spring-2023.local/',
+        hostname: 'ist-363-subaru-spring-2023.local',
       },
       {
-          protocol: 'https', 
+          protocol: 'https',
           hostname: 'subaru.ist363.xyz',
       }
     ],
